42e4f06855f885e45a59e98e332f921c15c008f2
yansfil <tansfil@gmail.com>
2021-12-16 11:36:53 +0900

    initialize

---
+++ app/__main__.py
@@ -0,0 +1,13 @@
+import os
+
+from fastapi import FastAPI, __version__
+
+app = FastAPI(
+    title="Jupyterhub kube monitoring service",
+    version=__version__,
+    ### Add our service client id to the /docs Authorize form automatically
+    swagger_ui_init_oauth={"clientId": os.environ["JUPYTERHUB_CLIENT_ID"]},
+    ### Default /docs/oauth2 redirect will cause Hub
+    ### to raise oauth2 redirect uri mismatch errors
+    swagger_ui_oauth2_redirect_url=os.environ["JUPYTERHUB_OAUTH_CALLBACK_URL"],
+)
